77a97661ae4cb512af7622ece941dcdc72e16751
Li Xian <lt14430@my.bristol.ac.uk>
2019-07-26 11:51:35 +0800

    hi

---
+++ StationeryStore/DB/Data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StationeryStore.DB
+{
+    public class Data
+    {
+        public static string strCN = "Server=Localhost;" +
+         "Database=LAPS; Integrated Security=true";
+    }
+}
