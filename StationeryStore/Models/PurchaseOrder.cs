cce12c9f4450ef0d70c8d937ea124fcc8eca0946
liuyang27 <liuyangvip1127@gmail.com>
2019-07-26 14:00:29 +0800

    added 4 classes

---
+++ StationeryStore/Models/PurchaseOrder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StationeryStore.Models
+{
+    public class PurchaseOrder
+    {
+        private string id;
+        private string supplierId;
+        private string orderedBy;
+        private string status;
+        private DateTime dateCreated;
+        private DateTime dateDelivery;
+
+        public string Id {
+            get { return id; }
+            set { id = value; }
+        }
+        public string SupplierId {
+            get { return supplierId; }
+            set { supplierId = value; }
+        }
+        public string OrderedBy {
+            get { return orderedBy; }
+            set { orderedBy = value; }
+        }
+        public string Status {
+            get { return status; }
+            set { status = value; }
+        }
+        public DateTime DateCreated {
+            get { return dateCreated; }
+            set { dateCreated = value; }
+        }
+        public DateTime DateDelivery {
+            get { return dateDelivery; }
+            set { dateDelivery = value; }
+        }
+    }
+}
