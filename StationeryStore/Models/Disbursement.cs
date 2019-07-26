793a4c15af0922a88036d4959d40caab692c015d
wanghongtaonus <49244238+wanghongtaonus@users.noreply.github.com>
2019-07-26 13:55:03 +0800

    disbursement and disbursementdetails model commit

---
+++ StationeryStore/Models/Disbursement.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StationeryStore.Models
+{
+    public class Disbursement
+    {
+        private string id;
+        private string departmentId;
+        private int collectionPoint;
+        private string status;
+        private DateTime dateCreated;
+        private string collectedBy;
+        private string remarks;
+        private string otp;
+        private string img;
+
+
+
+
+
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string DepartmentId
+        {
+            get { return departmentId; }
+            set { departmentId = value; }
+        }
+
+        public int CollectionPoint
+        {
+            get { return collectionPoint; }
+            set { collectionPoint = value; }
+        }
+
+        public DateTime DateCreated
+        {
+            get { return dateCreated; }
+            set { dateCreated = value; }
+        }
+
+        public string CollectedBy
+        {
+            get { return collectedBy; }
+            set { collectedBy = value; }
+        }
+
+        public string Remarks
+        {
+            get { return remarks; }
+            set { remarks = value; }
+        }
+
+        public string Otp
+        {
+            get { return otp; }
+            set { otp = value; }
+        }
+
+        public string Img
+        {
+            get { return img; }
+            set { img = value; }
+        }
+    }
+}
---
+++ StationeryStore/Models/DisbursementDetails.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StationeryStore.Models
+{
+    public class DisbursementDetails
+    {
+        private string id;
+        private string itemId;
+        private int qtyRetrieved;
+        private int qtyCollected;
+        
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string ItemId
+        {
+            get { return itemId; }
+            set { itemId = value; }
+        }
+        public int QtyRetrieved
+        {
+            get { return qtyRetrieved; }
+            set { qtyRetrieved = value; }
+        }
+        public int QtyCollected
+        {
+            get { return qtyCollected; }
+            set { qtyCollected = value; }
+        }
+       
+        
+    }
+}
